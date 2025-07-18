84f1eac041eaa2c22a200cf492912f548bd33355
OR <orazulay420@gmail.com>
2025-07-18 13:11:48 +0300

    created a scenarios file

---
+++ krav_maga_scenarios.py
@@ -0,0 +1,29 @@
+def push_from_behind():
+    print("\n  Scenario: Someone pushes you from behind  \n")
+    print("Situation: You are walking and suddenly someone pushes you from behind.")
+    print("\nRecommended Response:")
+    print("1. Use your stronger leg to step forward and regain your balance.")
+    print("2. Quickly turn around with your hands raised in a defensive posture.")
+    print("3. If necessary, deliver two controlled strikes aimed at creating space, not causing harm.")
+    print("4. Escape the situation as soon as it is safe to do so.\n")
+    input("Press Enter to continue...")
+
+def scenario_wrist_grab():
+    print("Scenario: Someone grabs your wrist.")
+    print("1. Rotate wrist toward the thumb.")
+    print("2. Pull your arm back.")
+    print("3. Step back defensively.")
+    input("Press Enter to continue...")
+
+def knife_to_throat_from_side():
+    print("\n  Scenario: Knife Threat to the Throat from the Side  \n")
+    print("Situation: An attacker is holding a knife to your throat from the side, threatening your life.")
+    print("\nRecommended Response:")
+    print("1. Stay calm and avoid sudden movements to prevent escalation.")
+    print("2. Quickly rotate your body toward the attacker to reduce the blade's pressure on your neck.")
+    print("3. Simultaneously grab the attacker's weapon hand with both of your hands to gain control.")
+    print("4. Redirect the knife away from your throat and deliver quick strikes (e.g., elbow, knee).")
+    print("5. Continue with control techniques or disengage and escape, depending on the situation.\n")
+    input("Press Enter to continue...")
+
+
